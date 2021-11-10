9b883e3456a488df1fec637e8b6b12b7f6409e56
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-10 10:34:19 -0500

    prevent default for trigger

--- src/UserInfoForm.js
+++ src/UserInfoForm.js
@@ -3,19 +3,20 @@ import { Component } from 'react';
 
 class UserInfoForm extends Component {
 
-  handleFormSubmit=() => {
+  handleFormSubmit=(event) => {
+    event.preventDefault();
     console.log("trigger");
   }
 
-    render(){
-      return (
-        <form onSubmit={this.handleFormSubmit} className="form-container">
-          <h2>User Information</h2>
-          
-          <button type="submit">Submit</button>
-        </form>
-      )
-    }
+  render(){
+    return (
+      <form onSubmit={this.handleFormSubmit} className="form-container">
+        <h2>User Information</h2>
+        
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
 }
 
 export default UserInfoForm;
