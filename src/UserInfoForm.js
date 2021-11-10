a6ef73588557bc4615f2da00f21d200831d6a0d8
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-10 10:59:43 -0500

    start notARobot as false

--- src/UserInfoForm.js
+++ src/UserInfoForm.js
@@ -1,18 +1,39 @@
 import { Component } from 'react';
 
-
 class UserInfoForm extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      notARobot: false,
+    }
+  }
 
   handleFormSubmit=(event) => {
     event.preventDefault();
-    console.log("trigger");
+    console.log("Submit trigger");
+  }
+
+  handleCheckboxChange=() => {
+    console.log("Checkbox trigger")
   }
 
   render(){
+    console.log(this.state)
+
     return (
       <form onSubmit={this.handleFormSubmit} className="form-container">
         <h2>User Information</h2>
         
+        {/* Make a checkbox */}
+        <input 
+          id="not-robot"
+          type="checkbox" 
+          // checked={}
+          onChange={this.handleCheckboxChange}
+        />
+
+
         <button type="submit">Submit</button>
       </form>
     )
